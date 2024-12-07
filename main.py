cb13c85da8e5f3bfda24b40ba7d322a52b31162c
neuronlearning <aekesn@gmail.com>
2024-12-07 23:35:26 +0100

    Database is now encrypted by default, Major changes

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ import string
 
 class DPManager:
 
-    def __init__(self,db_name):
+    temp_db = None
+    def __init__(self,db_name,db_encryption_key = ""):
         self.db_name = db_name
+        self.db_encryption_key = self.set_encryption_key(password=db_encryption_key)
 
     def encrypt(self,text:bytes,password:bytes):
         password = hashlib.sha256(password).digest()
@@ -20,54 +22,68 @@ class DPManager:
 
     def decrypt(self,text:bytes,password:bytes):
         password = hashlib.sha256(password).digest()
-
         cipher = AES.new(key=password,mode=AES.MODE_GCM,nonce=text[:15])
         decrypted = cipher.decrypt_and_verify(ciphertext=text[15:-16],received_mac_tag=text[-16:])
         return decrypted
 
-    def generate_database_key(self,length = 16):
+    def set_encryption_key(self,length = 16,password = ""):
+        if password:
+            return password.encode()
         alphabet = list(string.ascii_letters + string.digits + string.punctuation)
         for i in range(10):
             random.shuffle(alphabet)
 
         password = "".join(alphabet[:length])
         open("database_key.txt","w").write(password)
-        return password
+        return password.encode()
 
-    def create_database(self):
-        database = sqlite3.connect(f"{self.db_name}_db.db")
+    def create_database(self,):
+        database = sqlite3.connect(f"{self.db_name}.db")
         cursor = database.cursor()
         try:
             cursor.execute("CREATE TABLE credentials (id int,url varchar(512), username varchar(655), password MEDIUMTEXT, date_created varchar(64), date_updated varchar(64))")
             database.commit()
+            self.encrypt_database(self.db_encryption_key)
             return 1
         except sqlite3.OperationalError:
             pass
 
 
     def load_database(self):
-        if not os.path.isfile(f"{self.db_name}_db.db"):
+        if not os.path.isfile(f"{self.db_name}.db"):
             return None
-        database = sqlite3.connect(f"{self.db_name}_db.db")
-        return database
+        with open(f"{self.db_name}.db","rb") as file:
+            database = self.decrypt(file.read(),self.db_encryption_key)
+
+        with open(f"temp_db_file_{self.db_name}.temp", "wb") as file:
+            file.write(database)
+            file.close()
+
+        database = sqlite3.connect(f"temp_db_file_{self.db_name}.temp")
+        self.temp_db = database
+
+
+    def delete_temp_file(self):
+        os.remove(f"temp_db_file_{self.db_name}.temp")
+
+    def write_to_database(self):
+        with open(f"{self.db_name}.db", "wb") as file:
+            with open(f"temp_db_file_{self.db_name}.temp","rb") as file2:
+                file.write(self.encrypt(file2.read(),self.db_encryption_key))
+                file2.close()
+        self.temp_db.close()
+        self.delete_temp_file()
+
 
     def encrypt_database(self,key:bytes):
-        with open(f"{self.db_name}_db.db","rb") as file:
+        with open(f"{self.db_name}.db","rb") as file:
             unencrypted = file.read()
         encrypted_database = self.encrypt(unencrypted,key)
-        with open(f"{self.db_name}_db_encrypted.db","wb") as file:
+        with open(f"{self.db_name}.db","wb") as file:
             file.write(encrypted_database)
 
-    def decrypt_database(self,key:bytes):
-        with open(f"{self.db_name}_db_encrypted.db","rb") as file:
-            encrypted = file.read()
-        decrypted_database = self.decrypt(encrypted,key)
-        with open(f"{self.db_name}_db_decrypted_temp.db","wb") as file:
-            file.write(decrypted_database)
-
-
     def list_all_credentials(self):
-        database = self.load_database()
+        database = self.temp_db
         cursor = database.cursor()
 
         cursor.execute(f'SELECT id,url,username,password FROM credentials ')
@@ -77,8 +93,9 @@ class DPManager:
         for item in credentials:
             print(f"ID: {item[0]}\nURL: {item[1]}\nUsername: {item[2]}\nPassword: {item[3]}\n{"-" * 30}")
 
+
     def search_through_credentials(self,query:str):
-        database = self.load_database()
+        database = self.temp_db
         cursor = database.cursor()
 
         cursor.execute(f'SELECT id,url,username,password FROM credentials WHERE credentials.url LIKE "%{query}%" OR credentials.username LIKE "%{query}%" OR credentials.password LIKE "%{query}%"')
@@ -90,7 +107,7 @@ class DPManager:
 
 
     def check_if_credentials_exist(self, url: str, username: str, password: str):
-        database = self.load_database()
+        database = self.temp_db
         cursor = database.cursor()
 
         if cursor.execute(f'SELECT id,url,username,password '
@@ -102,7 +119,7 @@ class DPManager:
             return None
 
     def update_credentials_by_name_and_password(self,url:str,username:str,password:str,new_username:str,new_password:str):
-        database = self.load_database()
+        database = self.temp_db
         cursor = database.cursor()
         current_date = datetime.datetime.now()
         if new_password and new_username:
@@ -118,7 +135,7 @@ class DPManager:
         database.commit()
 
     def update_credentials_by_id(self,id:int,new_username:str,new_password:str):
-        database = self.load_database()
+        database = self.temp_db
         cursor = database.cursor()
         current_date = datetime.datetime.now()
         if new_password and new_username:
@@ -135,7 +152,7 @@ class DPManager:
 
 
     def add_credentials(self,url:str,username:str,password:str):
-        database = self.load_database()
+        database = self.temp_db
         cursor = database.cursor()
 
         if_exist = self.check_if_credentials_exist(url,username,password)
@@ -154,5 +171,9 @@ class DPManager:
 
 
 
-databa = DPManager("john")
-databa.create_database()
+databa = DPManager("adada","test")
+
+databa.load_database()
+databa.search_through_credentials("17")
+databa.write_to_database()
+
